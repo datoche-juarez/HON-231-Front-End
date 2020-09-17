e6386ee3c1f392a768640e91902587854549760b
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2020-09-17 19:52:59 -0400

    Home, about, contact, etc.

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const About = () =>  (
+        <div>
+            <h2>About Page</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis tortor sed libero sollicitudin lacinia quis at massa. In interdum</p>
+            <p>magna faucibus ipsum sagittis rutrum. Proin finibus viverra faucibus. Nullam ac felis id quam pellentesque vestibulum. Duis maximus</p>
+            <p>tortor sit amet risus volutpat, at venenatis orci faucibus. Donec tincidunt vulputate lobortis. Cras eu libero id neque faucibus ullamcorper</p>
+            <p>non sed turpis. Phasellus hendrerit bibendum lectus, non varius dolor volutpat ac. Aliquam id facilisis lorem. Integer euismod, libero a</p>
+            <p>aliquet sollicitudin, nisl turpis accumsan nunc, id volutpat mauris metus a dolor. Praesent molestie luctus quam, nec eleifend mi bibendum</p>
+            <p>vel.</p>
+        </div>
+    )
