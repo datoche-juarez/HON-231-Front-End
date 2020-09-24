443e6ff4e8576c167c724523ba904c8fed36de5e
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2020-09-23 22:37:11 -0400

    Updated About Page

--- src/About.js
+++ src/About.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 export const About = () =>  (
         <div>
-            <h2>About Page</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis tortor sed libero sollicitudin lacinia quis at massa. In interdum</p>
-            <p>magna faucibus ipsum sagittis rutrum. Proin finibus viverra faucibus. Nullam ac felis id quam pellentesque vestibulum. Duis maximus</p>
-            <p>tortor sit amet risus volutpat, at venenatis orci faucibus. Donec tincidunt vulputate lobortis. Cras eu libero id neque faucibus ullamcorper</p>
-            <p>non sed turpis. Phasellus hendrerit bibendum lectus, non varius dolor volutpat ac. Aliquam id facilisis lorem. Integer euismod, libero a</p>
-            <p>aliquet sollicitudin, nisl turpis accumsan nunc, id volutpat mauris metus a dolor. Praesent molestie luctus quam, nec eleifend mi bibendum</p>
-            <p>vel.</p>
+            <h1>About Page:</h1>
+
+<p>Our mission is to remove the barriers that stand between those that have had interactions with the criminal justice system and the opportunity to pursuit a healthy life, specifically employment opportunities and housing. The denial of these opportunities to those that have repaid their debt to society is not only cruel, it actively inhibits the reintegration process. This destructive cycle does not function, not for the individual, and not for our communities. </p>
+
+<p>At a time when millions of Americans are asking what they can do to be a part of the solution, and are looking for opportunities to work towards tangible results, we are working to create a space for those who have previously been denied the most basic of freedoms to connect with places of business and landlords who have made a commitment to move beyond denying opportunities to those with criminal records. Our hope is that everyone with the desire and the willingness has the chance to better their lives and that our communities will reap the rewards of healthier, more inclusive, and more productive society.</p>
         </div>
     )
