03ee0509beaf19bc2f121b979746aa101ac603a4
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2020-09-24 00:09:34 -0400

    Reformatted the text in <p> tags on About Page to make html easier to read...

--- src/About.js
+++ src/About.js
@@ -4,8 +4,22 @@ export const About = () =>  (
         <div>
             <h1>About Page:</h1>
 
-<p>Our mission is to remove the barriers that stand between those that have had interactions with the criminal justice system and the opportunity to pursuit a healthy life, specifically employment opportunities and housing. The denial of these opportunities to those that have repaid their debt to society is not only cruel, it actively inhibits the reintegration process. This destructive cycle does not function, not for the individual, and not for our communities. </p>
+<p>Our mission is to remove the barriers that stand between those that have had
+     interactions with the criminal justice system and the opportunity to pursuit 
+     a healthy life, specifically employment opportunities and housing. The denial 
+     of these opportunities to those that have repaid their debt to society is not
+      only cruel, it actively inhibits the reintegration process. This destructive 
+      cycle does not function, not for the individual, and not for our communities. 
+      </p>
 
-<p>At a time when millions of Americans are asking what they can do to be a part of the solution, and are looking for opportunities to work towards tangible results, we are working to create a space for those who have previously been denied the most basic of freedoms to connect with places of business and landlords who have made a commitment to move beyond denying opportunities to those with criminal records. Our hope is that everyone with the desire and the willingness has the chance to better their lives and that our communities will reap the rewards of healthier, more inclusive, and more productive society.</p>
+<p>At a time when millions of Americans are asking what they can do to be a part of
+     the solution, and are looking for opportunities to work towards tangible results,
+      we are working to create a space for those who have previously been denied the 
+      most basic of freedoms to connect with places of business and landlords who have
+       made a commitment to move beyond denying opportunities to those with criminal
+        records. Our hope is that everyone with the desire and the willingness has the
+         chance to better their lives and that our communities will reap the rewards of
+          healthier, more inclusive, and more productive society.
+          </p>
         </div>
     )
