5cadce19770c16d003fbdeabe1893950c70c9fce
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2020-09-24 00:59:52 -0400

    Changed Nav Bar Title...

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -21,7 +21,7 @@ const Styles = styled.div`
 export const NavigationBar = () => (
     <Styles>
         <Navbar expand="lg" >
-            <Navbar.Brand href="/">Navigation Bar</Navbar.Brand>
+            <Navbar.Brand href="/">Dan's React Project</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
