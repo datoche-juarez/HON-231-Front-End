c8968449e1ac0bb982088ae6c30626eea9d080b8
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2020-09-30 17:55:51 -0400

    Updated Home.js and About.js...

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,15 @@ import React from 'react';
 export const Home = () =>  (
         <div>
             <h1>Home Page:</h1>
+            <p>
+    Should everyone with a criminal record really be serving a life sentence?
+    Does the punishment ever end? Does it discourage crime and make us safer,
+    or does it perpetuate our staggering recidivism rates? When will we end
+    this madness and what can we do to be a part of the solution? People
+    deserve a chance to change their lives and become active participants in
+    our society. By denying them these opportunities, we are discouraging
+    change and limiting their opportunity to pursue a better life.
+</p>
 <p>
     Although crime victimization rates have been decreasing in the United
     States, government spending on policing continues to increase.
@@ -10,7 +19,8 @@ export const Home = () =>  (
     lives of countless Americans, who after serving lengthy jail or prison
     sentences, completing probation or parole, and paying hefty fines and
     restitution, are still increasingly finding themselves pushed out of their
-    communities.
+    communities. Many employers and landlords have strict policies which
+    prevent former offenders from being able to secure employment or housing.
 </p>
 <p>
     We have all heard the shocking statistic: while only having 5% of the
@@ -23,12 +33,13 @@ export const Home = () =>  (
     are right, we will work towards criminal justice reform.
 </p>
 <p>
-    That time must be now. With renewed support for criminal justice reform,
-    and during a time of national reflection around the issues of policing and
-    crime, we are presented with an opportunity to re-envision a rational and
-    functional response to crime, one in which justice and rehabilitation are
-    centered, and where the goal is freedom, for our communities, and those
-    that have worked so, so hard but have long had it denied to them.
+    The time for action is now. With renewed support for criminal justice
+    reform, and during a time of national reflection around the issues of
+    policing and crime, we are presented with an opportunity to re-envision a
+    rational and functional response to crime, one in which justice and
+    rehabilitation are centered, and where the goal is freedom, for our
+    communities, and those that have worked hard but have long had it denied to
+    them.
 </p>
         </div>
     )
