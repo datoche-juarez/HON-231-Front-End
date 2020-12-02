a7673eab9ae770cc781de7f7971803f9bf08ce8c
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2020-12-02 18:44:55 -0500

    Changed color layout for contact title and added contact page information (Contact.js)...

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+const styleh2 = {color: '#FF6347'};
+
+
 export const Contact = () =>  (
         <div>
-            <h2>Contact Us</h2>
+            <h2 style= {styleh2}>Contact Us</h2>
             <h3>Phone:</h3>
             <p>313-675-2860</p>
             <h3>Email:</h3>
             <p>datoche-juarez@hawkmail.hfcc.edu</p>
             <h3>Facebook:</h3>
             <p>https://www.facebook.com/daniel.atochejuarez/</p>
+            <p>The Way Forward Project is only as good as its partners. If you believe our current criminal
+             justice system is broken and recognize the need to give millions of Americans the opportunity 
+             to lead productive healthy lives, please consider becoming one of our first partners. In the 
+             year 2020, more than 20 years after the passing of the 1994 Crime Bill, its failure has become 
+             obvious to the vast majority Americans, so much so in fact that criminal justice reform has 
+             become popular across the political spectrum. Because of the growing interest to fixing these 
+             problems, we hope to see a lot of community engagement.</p>
+            <p>If you own or manage a place of business and are interested in having your business or 
+             organization&rsquo;s name listed alongside others in the community that have made a commitment
+             to ending the practice of blanket discrimination based on a candidates criminal record, please 
+             fill out our contact form and we will add you to our job listings. If you own or manage housing 
+             or rental properties and are committed to ending the practice of housing discrimination which 
+             often keeps former offenders in a constant state of housing insecurity, please leave us your
+             contact information. &nbsp;</p>
+            <p>If you believe there is a better, smarter, more humane, and compassionate way forward, please 
+             fill out our contact form and become a part of the solution. Upon completion of our form, we will 
+             send you confirmation that we received your contact information. If you at any time would like to 
+             change or remove your organization&rsquo;s data, please contact us.</p>
         </div>
     )
